a6ba739bb3147bd0613c4f91b0acb07934c62e4b
dewansh vishwakarma <dewanshvishwakarma073@gmail.com>
2026-09-23 06:36:44 +0530

    basic create databse and table

---
+++ sql basic.sql
@@ -0,0 +1,8 @@
+create database d1;
+use d1;
+create table employee(eid int , name varchar(50), mob int(12), salary float);
+select * from employee;
+
+insert into employee values(1,"ram",8103349,5000);
+
+select* from employee;
